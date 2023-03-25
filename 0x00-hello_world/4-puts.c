3b929b3cdd5843c6560f30bbf0df27913b19e2b4
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-03-25 02:27:04 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A programme that prints a line with pputs function
+ * Return: 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl");
+	return (0);
+}
