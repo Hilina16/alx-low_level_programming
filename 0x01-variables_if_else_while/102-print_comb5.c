680400b1cbb2b19a684af73cd35dbbd677d8744e
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-03-26 12:08:43 -0700

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,10 +20,10 @@ int main(void)
 				putchar((d2 / 10) + 48);
 				putchar((d2 % 10) + 48);
 				if (d1 != 98 || d2 != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			d2++;
 		}
