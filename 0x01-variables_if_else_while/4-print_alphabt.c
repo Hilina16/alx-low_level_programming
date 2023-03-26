cc006e7d629c3be864b19a90157f3f22044274c6
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-03-25 19:15:51 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - alphabet without q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'e' || lowercase == 'q')
+		{
+			lowercase += 1;
+		}
+		else
+		{
+			putchar(lowercase);
+			lowercase += 1;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
