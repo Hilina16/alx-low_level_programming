b4afac72a148ded33cb6ef6bb6487c40b247a710
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-03-30 10:14:54 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,20 +15,23 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("Fizz Buzz");
 		}
 		else if ((n % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((n % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
+		if (n == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
