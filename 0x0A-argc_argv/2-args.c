11b6586c50fc29ba4d4b7941dbd4ddafaec7f8e5
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-04-12 20:24:08 -0700

    ./args You can do anything, but not everything.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives one per line
+ * @argc: count number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+
+	for (c = 0; c < argc; c++)
+	{
+		printf("%s\n", argv[c]);
+	}
+	return (0);
+}
