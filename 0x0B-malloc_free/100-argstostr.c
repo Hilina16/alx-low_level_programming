1c548a6471025c3c4fd3475636bc3acaae7baa53
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-04-17 05:21:34 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe, concatnate everystringin the program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: input value
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int x, y;
+	int r = 0, l = 0;
+	char *str;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y]; y++)
+			l++;
+	}
+	l += ac;
+	str = malloc(sizeof(char) * l + 1);
+
+	if (str == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y]; y++)
+		{
+			str[r] = av[x][y];
+			r++;
+		}
+		if (str[r] == '\0')
+			str[r++] = '\n';
+	}
+	return (str);
+}
