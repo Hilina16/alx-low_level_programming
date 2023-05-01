5aedc7ebc09b63f300a17138f639a9713ef7d88d
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-05-01 02:08:34 -0700

    free the allocated memory

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,4 +5,16 @@
 /**
  * free_listint - function that frees a listint_t list.
  * @head: the head pointer
- *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
