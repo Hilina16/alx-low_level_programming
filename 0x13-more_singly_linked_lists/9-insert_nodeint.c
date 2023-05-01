6ca5954cbd4f1c04af2ed8872dfa9516ee3a792d
Hilina16 <hilinagebrewahd2017@gmail.com>
2023-05-01 10:35:21 -0700

    adding a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,21 +11,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 	listint_t *new_node;
 	unsigned int x;
 
 	if (head == NULL)
 		return (NULL);
+	if (idx != 0)
+	{
+		tmp = *head;
+		for (x = 0; x < idx - 1 && tmp != NULL; x++)
+			tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (tmp)
+	new_node->n = n;
+
+	if (idx == 0)
 	{
-		for (x = 0; x < idx; x++)
-			tmp = tmp->next;
-		new_node = tmp->next;
-		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
+	new_node->next = tmp->next;
+	tmp->next = new_node;
 	return (new_node);
 }
